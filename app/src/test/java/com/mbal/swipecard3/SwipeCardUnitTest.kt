bc9fc37aea10e924efff61b57ec8f553ec7e3188
BALMehmet <79014441+BALMehmet@users.noreply.github.com>
2021-03-14 00:02:57 +0300

    Update SwipeCardUnitTest.kt

--- app/src/test/java/com/mbal/swipecard3/SwipeCardUnitTest.kt
+++ app/src/test/java/com/mbal/swipecard3/SwipeCardUnitTest.kt
@@ -1,160 +1,99 @@
 package com.mbal.swipecard3
 
-import android.view.View
-import android.view.animation.LinearInterpolator
+import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.*
-import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.mbal.swipecard3.adaptors.ProfilesAdapter
 import com.mbal.swipecard3.databinding.ActivityMainBinding
+import com.mbal.swipecard3.databinding.ProfileCardStackBinding
 import com.mbal.swipecard3.model.CharacterList
 import com.mbal.swipecard3.service.RickAndMortyApi
 import com.mbal.swipecard3.service.RickAndMortyInterface
 import com.mbal.swipecard3.viewmodel.CharacterListViewModel
-import com.yuyakaido.android.cardstackview.CardStackLayoutManager
-import com.yuyakaido.android.cardstackview.CardStackView
-import com.yuyakaido.android.cardstackview.Direction
-import com.yuyakaido.android.cardstackview.SwipeableMethod
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import android.os.Bundle as OsBundle
+import java.io.IOException
 
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class SwipeCardUnitTest {
+//Unit Test..
+class SwipeCardUnitTest{
+
     @Test
     fun apiService_isGetCharacters() {
         var rickAndMortyInterface = RickAndMortyApi().getClient()!!.create(RickAndMortyInterface::class.java);
-        rickAndMortyInterface.getCharacters("character?page=1").enqueue(object : Callback<CharacterList>{
-            override fun onFailure(call: Call<CharacterList>, t: Throwable) {
+        var apiResponse:CharacterList
+        try {
+            apiResponse=rickAndMortyInterface.getCharacters("character?page=1").execute().body()!!
+            assertEquals(20, apiResponse.results.size)
+        }
 
-            }
-
-            override fun onResponse(call: Call<CharacterList>, response: Response<CharacterList>) {
-                assertEquals(response.body()!!.results.size, 20)
-            }
-
-        })
+        catch (e: IOException) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     fun id_isSuitModel() {
-        var rickAndMortyInterface = RickAndMortyApi().getClient()!!.create(RickAndMortyInterface::class.java);
-        rickAndMortyInterface.getCharacters("character?page=1").enqueue(object : Callback<CharacterList>{
-            override fun onFailure(call: Call<CharacterList>, t: Throwable) {
-
+        fun apiService_isGetCharacters() {
+            var rickAndMortyInterface = RickAndMortyApi().getClient()!!.create(RickAndMortyInterface::class.java);
+            var apiResponse:CharacterList
+            try {
+                apiResponse=rickAndMortyInterface.getCharacters("character?page=1").execute().body()!!
+                assertEquals(1, apiResponse.results[0].id)
             }
 
-            override fun onResponse(call: Call<CharacterList>, response: Response<CharacterList>) {
-                assertEquals(response.body()!!.results[0].id, 1)
+            catch (e: IOException) {
+                e.printStackTrace();
             }
-
-        })
+        }
     }
 
     @Test
     fun name_isSuitModel() {
         var rickAndMortyInterface = RickAndMortyApi().getClient()!!.create(RickAndMortyInterface::class.java);
-        rickAndMortyInterface.getCharacters("character?page=1").enqueue(object : Callback<CharacterList>{
-            override fun onFailure(call: Call<CharacterList>, t: Throwable) {
+        var apiResponse:CharacterList
+        try {
+            apiResponse=rickAndMortyInterface.getCharacters("character?page=1").execute().body()!!
+            assertEquals("Rick Sanchez", apiResponse.results[0].name)
+        }
 
-            }
-
-            override fun onResponse(call: Call<CharacterList>, response: Response<CharacterList>) {
-                assertEquals(response.body()!!.results[0].name, "Rick Sanchez")
-            }
-
-        })
+        catch (e: IOException) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     fun status_isSuitModel() {
         var rickAndMortyInterface = RickAndMortyApi().getClient()!!.create(RickAndMortyInterface::class.java);
-        rickAndMortyInterface.getCharacters("character?page=1").enqueue(object : Callback<CharacterList>{
-            override fun onFailure(call: Call<CharacterList>, t: Throwable) {
+        var apiResponse:CharacterList
+        try {
+            apiResponse=rickAndMortyInterface.getCharacters("character?page=1").execute().body()!!
+            assertEquals("Alive", apiResponse.results[0].status)
+        }
 
-            }
-
-            override fun onResponse(call: Call<CharacterList>, response: Response<CharacterList>) {
-                assertEquals(response.body()!!.results[0].status, "Alive")
-            }
-
-        })
+        catch (e: IOException) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     fun location_isSuitModel() {
         var rickAndMortyInterface = RickAndMortyApi().getClient()!!.create(RickAndMortyInterface::class.java);
-        rickAndMortyInterface.getCharacters("character?page=1").enqueue(object : Callback<CharacterList>{
-            override fun onFailure(call: Call<CharacterList>, t: Throwable) {
-
-            }
-
-            override fun onResponse(call: Call<CharacterList>, response: Response<CharacterList>) {
-                assertEquals(response.body()!!.results[0].location.name, "Earth")
-            }
-
-        })
-    }
-
-    /*@Test
-    fun card_isSwiped(){
-        var binding=ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        var layoutManager: CardStackLayoutManager
-        var charactersViewModel: CharacterListViewModel
-        val profilesAdapter = ProfilesAdapter()
-        var pageCounter =1
-
-        layoutManager = CardStackLayoutManager(this, this).apply {
-            setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
-            setOverlayInterpolator(LinearInterpolator())
+        var apiResponse:CharacterList
+        try {
+            apiResponse=rickAndMortyInterface.getCharacters("character?page=1").execute().body()!!
+            assertEquals("Earth (Replacement Dimension)", apiResponse.results[0].location.name)
+            print(apiResponse.results[0].location.name)
         }
-        charactersViewModel = ViewModelProvider(this).get(CharacterListViewModel::class.java)
-        charactersViewModel.GetCharacterListViewModel(pageCounter)!!.observe(this, Observer {
-            profilesAdapter.setCharacters(it)
-        })
 
-        binding.cardStackView.layoutManager=layoutManager
-        binding.cardStackView.adapter=profilesAdapter
-        binding.cardStackView.itemAnimator=DefaultItemAnimator()
-        binding.cardStackView.swipe()
-        binding.cardStackView.rewind()
-
-
+        catch (e: IOException) {
+            e.printStackTrace();
+        }
     }
-
-
-
-    override fun onCardDisappeared(view: View?, position: Int) {
-
-    }
-
-    override fun onCardDragging(direction: Direction?, ratio: Float) {
-
-    }
-
-    override fun onCardSwiped(direction: Direction?) {
-        assertEquals(true, true)
-    }
-
-    override fun onCardCanceled() {
-
-    }
-
-    override fun onCardAppeared(view: View?, position: Int) {
-
-    }
-
-    override fun onCardRewound() {
-
-    }*/
 }
 
